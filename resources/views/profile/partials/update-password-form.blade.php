b1503fa2ce7ee5ba611969119e93add266a03e36
WChristianPerera <christianperera13@gmail.com>
2023-08-29 10:17:09 +0200

    update password

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -13,33 +13,38 @@
         @csrf
         @method('put')
 
-        <div>
-            <label for="current_password">Current Password</label>
-            <input id="current_password" name="current_password" type="password">
-            @error('current_password')
-                {{ $message }}
-            @enderror
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="current_password">Current Password</label>
+              <input id="current_password" class="form-control is-valid"  placeholder="Current Password"  name="current_password" type="password" required>
+                @error('current_password')
+                    {{ $message }}
+                @enderror
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="password">New Password</label>
+              <input id="password" class="form-control is-valid" placeholder="New Password"  name="current_password" type="password" required>
+              <div class="valid-feedback">
+                Accettiamo solo bestemmie!
+              </div>
+                @error('current_password')
+                    {{ $message }}
+                @enderror
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="password_confirmation">Confirm Password</label>
+                <input id="password_confirmation" class="form-control is-validc"   name="password_confirmation" type="password" required>
+                @error('password_confirmation')
+                    {{ $message }}
+                @enderror
+            </div>
+
         </div>
-
-        <div>
-            <label for="password">New Password</label>
-            <input id="password" name="password" type="password">
-            @error('password')
-                {{ $message }}
-            @enderror
-        </div>
-
-        <div>
-            <label for="password_confirmation">Confirm Password</label>
-            <input id="password_confirmation" name="password_confirmation" type="password">
-            @error('password_confirmation')
-                {{ $message }}
-            @enderror
-        </div>
-
-
+          
+        
         <div class="flex items-center gap-4">
-            <button>Save</button>
+            <button class="btn btn-primary" type="submit">Save</button>
+
 
             @if (session('status') === 'password-updated')
                 <p>Saved.</p>
